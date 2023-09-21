f78496833789a24e465a3611758b874498c98b53
CodingbyDaniel <109753547+Dana-Codes@users.noreply.github.com>
2023-09-21 23:03:03 +0100

    allow

--- README.md
+++ README.md
@@ -1,18 +1,31 @@
-# simple_shell
-Building  a simple bash shell almost from scratch and limited number of system calls and functions
+Simple Shell Team Project
 
-Features
-Display a prompt and wait for the user to type a command. A command line always ends with a new line.
-If an executable cannot be found, print an error message and display the prompt again.
-Handle errors.
-Hndling the “end of file” condition (Ctrl+D)
-Hanling the command line with arguments
-Handle the PATH
-Support the exit features and the exit status
-Handle the Ctrl-C to not terminate the shell
-Handling the command seperator ;
-Handling && and || logical operators
-Handle variable replacements $? and $$
-Handle the comments #
-Support the history feature
-Support the file input
+Project was completed using
+C language
+Shell
+Betty linter
+
+General Requirement for project
+All files are compiled on Ubuntu 20.04 LTS using gcc, using the options -Wall -Werror -Wextra -pedantic -std=gnu89
+All files ends with a new line
+
+A README.md file at the root of the folder of the project is mandatory
+Used the Betty style to check using betty-style.pl and betty-doc.pl
+Shell should not have any memory leaks
+No more than 5 functions per file
+All header files should be include guarded
+Write a README with the description of the project
+Description
+hsh is a simple UNIX command language interpreter that reads commands from either a file or standard input and executes them.
+
+How hsh works
+Prints a prompt and waits for a command from the user
+Creates a child process in which the command is checked
+Checks for built-ins, aliases in the PATH, and local executable programs
+The child process is replaced by the command, which accepts arguments
+When the command is done, the program returns to the parent process and prints the prompt
+The program is ready to receive a new command
+To exit: press Ctrl-D or enter "exit" (with or without a status)
+Works also in non interactive mode
+Compilation
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 *.c -o hsh
--- main.c
+++ main.c
@@ -1,104 +1,134 @@
 #include "shell.h"
 
-extern char **environ;
+void sig_handler(int sig);
+int execute(char **args, char **front);
 
 /**
- * main - Build a simple shell like sh
+ * sig_handler - Prints a new prompt upon a signal.
+ * @sig: The signal.
+ */
+void sig_handler(int sig)
+{
+	char *new_prompt = "\n$ ";
+
+	(void)sig;
+	signal(SIGINT, sig_handler);
+	write(STDIN_FILENO, new_prompt, 3);
+}
+
+/**
+ * execute - Executes a command in a child process.
+ * @args: An array of arguments.
+ * @front: A double pointer to the beginning of args.
  *
- * Return: 0;
+ * Return: If an error occurs - a corresponding error code.
+ *         O/w - The exit value of the last executed command.
  */
-int main(void)
+int execute(char **args, char **front)
 {
-    int foo;
-    char *command = NULL;
-    size_t len = 0;
-    char **args = NULL;
+	pid_t child_pid;
+	int status, flag = 0, ret = 0;
+	char *command = args[0];
 
-    while (1)
-    {
-        if (isatty(0))
-            printf("$ ");
+	if (command[0] != '/' && command[0] != '.')
+	{
+		flag = 1;
+		command = get_location(command);
+	}
 
-        foo = getline(&command, &len, stdin);
-        if (foo == -1)
-        {
-            free(command);
-            exit(0);
-        }
-        command[foo - 1] = '\0';
-        args = split_string(command, " ");
-        if (args[0] == NULL || handle_args(args) != 0)
-        {
-            free(command);
-            command = NULL;
-            free(args);
-            continue;
-        }
-        _execute(args, command);
-        free_pointers_array(args);
-    }
-    free_pointers_array(args);
-    free(command);
-    return (0);
+	if (!command || (access(command, F_OK) == -1))
+	{
+		if (errno == EACCES)
+			ret = (create_error(args, 126));
+		else
+			ret = (create_error(args, 127));
+	}
+	else
+	{
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			if (flag)
+				free(command);
+			perror("Error child:");
+			return (1);
+		}
+		if (child_pid == 0)
+		{
+			execve(command, args, environ);
+			if (errno == EACCES)
+				ret = (create_error(args, 126));
+			free_env();
+			free_args(args, front);
+			free_alias_list(aliases);
+			_exit(ret);
+		}
+		else
+		{
+			wait(&status);
+			ret = WEXITSTATUS(status);
+		}
+	}
+	if (flag)
+		free(command);
+	return (ret);
 }
 
 /**
- * handle_args - handle special cases
- * @args: the argv array
- * Return: return value
+ * main - Runs a simple UNIX command interpreter.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: The return value of the last executed command.
  */
-int handle_args(char **args)
+int main(int argc, char *argv[])
 {
-    int i;
+	int ret = 0, retn;
+	int *exe_ret = &retn;
+	char *prompt = "$ ", *new_line = "\n";
 
-    if (strcmp(args[0], "exit") == 0)
-    {
-        if (args[1])
-            exit(atoi(args[1]));
-        exit(0);
-    }
-    else if (strcmp(args[0], "env") == 0)
-    {
-        for (i = 0; environ[i] != NULL; i++)
-            printf("%s\n", environ[i]);
-        return (1);
-    }
-    else
-        return (handle_path(args));
-}
-/**
- * _execute - execute the command
- * @args: the argv array
- * @command: the prompt the user typed
- * Return: nothing
- */
-void _execute(char **args, char *command)
-{
-    int foo;
-    pid_t pid;
+	name = argv[0];
+	hist = 1;
+	aliases = NULL;
+	signal(SIGINT, sig_handler);
 
-    pid = fork();
-    if (pid == -1)
-    {
-        free(command);
-        command = NULL;
-        free_pointers_array(args);
-        perror("Fork");
-        exit(errno);
-    }
-    else if (pid == 0)
-    {
-        foo = execve(args[0], args, environ);
-        if (foo == -1)
-        {
-            free(command);
-            free_pointers_array(args);
-            perror("./hsh: 1");
-            exit(errno);
-        }
-    }
-    else
-    {
-        wait(NULL);
-    }
+	*exe_ret = 0;
+	environ = _copyenv();
+	if (!environ)
+		exit(-100);
+
+	if (argc != 1)
+	{
+		ret = proc_file_commands(argv[1], exe_ret);
+		free_env();
+		free_alias_list(aliases);
+		return (*exe_ret);
+	}
+
+	if (!isatty(STDIN_FILENO))
+	{
+		while (ret != END_OF_FILE && ret != EXIT)
+			ret = handle_args(exe_ret);
+		free_env();
+		free_alias_list(aliases);
+		return (*exe_ret);
+	}
+
+	while (1)
+	{
+		write(STDOUT_FILENO, prompt, 2);
+		ret = handle_args(exe_ret);
+		if (ret == END_OF_FILE || ret == EXIT)
+		{
+			if (ret == END_OF_FILE)
+				write(STDOUT_FILENO, new_line, 1);
+			free_env();
+			free_alias_list(aliases);
+			exit(*exe_ret);
+		}
+	}
+
+	free_env();
+	free_alias_list(aliases);
+	return (*exe_ret);
 }
